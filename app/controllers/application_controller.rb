055de831dc4f04fee442db0b1364f6b7ef904698
Gonzalo Oyanedel Vial <goyanedelv@gmail.com>
2021-10-15 23:16:00 +0000

    full video

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  #layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+
+  end
 
   def play_rock
     # a ton of code
-
     #redirect_to("https://www.wikipedia.org")
     #render({ :plain => rand(100)})
     #render({ :html => "<h1>Hello, World!</h1>".html_safe})
@@ -14,4 +18,20 @@ class ApplicationController < ActionController::Base
       # template must be located in the app/views folder 
   end
 
+  def play_paper
+    @comp_move =  ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+    render({ :template => "game_templates/user_paper.html.erb"})
+  end 
+
+  def play_scissors
+    render({ :template => "game_templates/user_scissors.html.erb"})
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get("/",{:controller => "application", :action => "homepage"})
 
   get("/rock",{:controller => "application", :action => "play_rock"})
-  #get("/paper",{:controller => "", :action => ""})
-  #get("/scissor",{:controller => "", :action => ""})
+
+
+  get("/paper",{:controller => "application", :action => "play_paper"})
+  get("/scissors",{:controller => "application", :action => "play_scissors"})
+
 
 end

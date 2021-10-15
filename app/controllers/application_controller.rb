c5f368a1777347c8fa48d5c1b13f078d40737024
Gonzalo Oyanedel Vial <goyanedelv@gmail.com>
2021-10-15 22:47:16 +0000

    video until min 37

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    # a ton of code
+
+    #redirect_to("https://www.wikipedia.org")
+    #render({ :plain => rand(100)})
+    #render({ :html => "<h1>Hello, World!</h1>".html_safe})
+    render({ :template => "game_templates/user_rock.html.erb"})
+      # template must be located in the app/views folder 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/rock",{:controller => "application", :action => "play_rock"})
+  #get("/paper",{:controller => "", :action => ""})
+  #get("/scissor",{:controller => "", :action => ""})
 
 end
